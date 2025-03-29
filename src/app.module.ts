b6c6c9190b0617534d3dc5628343051f18e9eab0
ArunKumar2331996 <231996arunkumar@gmail.com>
2025-03-29 14:25:28 +0530

    Main branch changes.

---
+++ src/app.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
+import { User } from './user/entities/user.entity';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      password: 'postgres',
+      username: 'postgres',
+      entities: [User],
+      database: 'postgres',
+      synchronize: true,
+      logging: true,
+    }),
+    UserModule,
+    //UserModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService]
+})
+export class AppModule {}
